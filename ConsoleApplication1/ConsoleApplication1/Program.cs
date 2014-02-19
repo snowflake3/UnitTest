1b3cf5dd06a144c7c1eceffb4029b14a28415345
snowflake3 <tariqual.hassan@kaz.com.bd>
2014-02-19 16:07:01 +0600

    Console Application with Unit testing
    this is the first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System; // for the console object
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+  
+
+class addtwonumbers
+{
+    public static int addIntegers(int a, int b)
+    {
+        return (a + b);
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            // output to the console
+            Console.Write("Please enter value 1 : ");
+            // readin the console input, and then convert to a integer value
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter value 2 : ");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            // write out the answer 
+            Console.WriteLine("Answer = " + addIntegers(val1, val2));
+        }
+        catch (Exception e)
+        {
+            // any errors. Like converting a string to a integer value
+            Console.WriteLine("Error : " + e.ToString());
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestLibrary1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestLibrary1
+{
+    [TestClass]
+    public class addtwonumbers
+    {
+        [TestMethod]
+        public void addIntegers()
+        {
+            Assert.AreEqual(10, 5 + 5);
+        }
+    }
+}
